7b399a3772b1fffa9dc6502035ea4343fadf4e14
Levi Athans <racyash@gmail.com>
2017-07-22 12:46:37 +0800

    Replace index.html with index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Acuity</title>
+	<link rel="stylesheet" href="./assets/css/style.css">
+	<script src="./assets/js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="./assets/js/vendor/bootstrap/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="./assets/css/vendor/bootstrap/bootstrap.css">
+</head>
+<body>
+	<script type="text/javascript">
+		var chubbies_json = {};
+	</script>
+	<div id="video_container">
+		<div id="video_wrapper">
+			<div id="products_wrapper">
+				<div id="products_cart" class="glyphicon glyphicon-shopping-cart"></div>
+			</div>
+			<video id="video">
+				<source src="assets/videos/chubbies-marshup-2017.mp4" type='video/mp4;'>
+			</video>
+		</div>
+	</div>
+	<div class="modal fade" id="product_modal" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<span id="product_name"></span>
+					$<span id="product_price"></span>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-primary" id="add_to_cart" data-dismiss="modal">Add to Cart</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="checkout_modal" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">Checkout</div>
+				<div class="modal-body">
+					You've added <span id="cart_items"></span> to the shopping cart
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-primary" id="checkout" data-dismiss="modal">Checkout</button>
+					<button type="button" class="btn btn-default" id="replay_button" data-dismiss="modal"><span class="glyphicon glyphicon-repeat"></span></button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="./assets/js/pages/video.js" type="text/javascript"></script>
+	<script src="./assets/js/pages/chubbies_data.js" type="text/javascript"></script>
+</body>
+</html>
