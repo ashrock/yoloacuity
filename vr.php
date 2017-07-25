9e784f16cbbbc3131eed80738223e8451e74c700
Levi Athans <racyash@gmail.com>
2017-07-25 20:56:15 +0800

    Iframe VR app

---
+++ vr.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Acuity VR</title>
+	<link rel="stylesheet" href="./assets/css/style.css">
+	<script src="./assets/js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="./assets/js/vendor/bootstrap/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="./assets/css/vendor/bootstrap/bootstrap.css">
+</head>
+<body>
+	<div id="wrapper">
+		<iframe src="http://acuity.village88.com/" width="900" height="506.25" frameborder="0"></iframe>
+	</div>
+	<div id="warning-message">
+		<div id="mobile_message">This website is only viewable in landscape mode</div>
+	</div>
+</body>
+</html>
