26bf5fcf7554619602c12a6e494396f39a2d662e
Levi Athans <racyash@gmail.com>
2017-08-14 14:06:49 +0800

    Implement checkout page simulations 2

--- checkout.php
+++ checkout.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
-	<meta charset="UTF-8">
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	<title>Acuity | Checkout</title>
 	<link rel="stylesheet" href="./assets/css/vendor/bootstrap/bootstrap.css">
