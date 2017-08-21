db81df779acb8fc1bc386135a1626c0b2dec2947
Levi Athans <racyash@gmail.com>
2017-08-21 14:17:00 +0800

    Change password for Acuity

--- check_password.php
+++ check_password.php
@@ -2,7 +2,7 @@
 	$json_array = array('status' => FALSE);
 	if($_POST)
 	{
-		if(array_key_exists('password', $_POST) && $_POST['password'] == "duHcycsHbHM92yNR")
+		if(array_key_exists('password', $_POST) && ($_POST['password'] == "duHcycsHbHM92yNR" OR $_POST['password'] == "AcuityAI"))
 		{
 			$json_array = array('status' => TRUE);
 		}
